94ffc774a49c5fc35b18e8e9a5522c6375c9e20d
Chemonin <iachemonin@gmail.com>
2019-10-05 16:45:25 +0300

    created card class

--- src/controllers/card-controller.js
+++ src/controllers/card-controller.js
@@ -1,2 +1,12 @@
 import Card from '../components/card.js';
 import EditCard from '../components/edit-card.js';
+import {Position, render} from '../utils.js';
+
+export default class CardController {
+  constructor(data) {
+    this._card = new Card(data);
+    this._editCard = new EditCard(data);
+  }
+
+
+}
