c15d2d804da0467ae45d5ae311a2d3c6dbde68a2
Chemonin <iachemonin@gmail.com>
2019-10-06 11:47:05 +0300

    added new placeholder form

--- src/components/contactCreator.js
+++ src/components/contactCreator.js
@@ -6,7 +6,7 @@ export default class ContactCreator extends AbstractElement {
       <label class='visually-hidden' for='new-name'>Name</label>
       <input type='text' id='new-name' name='new-name' placeholder='enter name' required>
       <label class='visually-hidden' for='new-phone'>Phone</label>
-      <input type='text' id='new-phone' name='new-phone' placeholder='enter phone' required>
+      <input type='text' id='new-phone' name='new-phone' placeholder='enter phone +7xxxxxxxxxx or 8xxxxxxxxxx' required>
       <button class='add-form__btn' type='submit'>Create</button>
     </form>`;
   }
